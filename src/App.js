f5a8ab085cbc219c55510313337e09668134be72
David Heidrich <me@bowlingx.com>
2015-05-10 05:27:05 +0200

    added some parallax to header

--- src/App.js
+++ src/App.js
@@ -6,17 +6,29 @@ global.document.addEventListener('DOMContentLoaded', () => {
     // Create of canvas navigation
     new OffCanvas('MainNavigation', 'SidebarDarkener', -1).registerEvents();
     // init forms
-    const header = document.getElementById('Header'), maxScroll = 150;
+    const header = document.getElementById('Header'),
+        logo = document.getElementById('Logo'), maxScroll = 150;
 
+    function easeInQuad(t, b, c, d) {
+        t /= d;
+        return c * t * t + b;
+    }
 
     function initScroll() {
         requestAnimationFrame(() => {
             let scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-            scrollTop = Math.max(scrollTop < maxScroll? scrollTop : maxScroll,0);
-                header.style.cssText += `
+            scrollTop = Math.max(scrollTop < maxScroll ? scrollTop : maxScroll, 0);
+            header.style.cssText += `
                 transform:translate3d(0,-${scrollTop}px,0);
                 -webkit-transform:translate3d(0,-${scrollTop}px,0);`;
-        })
+            const opacity = 1 - easeInQuad(scrollTop, 0, 1, maxScroll),
+                move = easeInQuad(scrollTop, 0, 50, maxScroll);
+
+            logo.style.cssText += `opacity:${opacity};
+                transform:translate3d(0,${move}px,0);
+                -webkit-transform:translate3d(0,${move}px,0);
+                `;
+        });
     }
 
     initScroll();
