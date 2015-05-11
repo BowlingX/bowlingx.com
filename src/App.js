3d0f2c067c80d561d3230f42c0a018bd77c09037
David Heidrich <me@bowlingx.com>
2015-05-11 02:03:37 +0200

    finished overall layout big version subpages

--- src/App.js
+++ src/App.js
@@ -5,33 +5,59 @@ global.document.addEventListener('DOMContentLoaded', () => {
 
     // Create of canvas navigation
     new OffCanvas('MainNavigation', 'SidebarDarkener', -1).registerEvents();
-    // init forms
-    const header = document.getElementById('Header'),
-        logo = document.getElementById('Logo'), maxScroll = 150;
 
+    // init forms
+    const header          = document.getElementById('Header'),
+        logoBar           = document.getElementById('Logo-bar'),
+        logo              = document.getElementById('Logo'),
+        maxScroll         = 150,
+        desktopBreakpoint = 768,
+        shouldScroll      = () => {
+            return window.innerWidth >= desktopBreakpoint;
+        };
+
+    // easing function to use for parallax effects
     function easeInQuad(t, b, c, d) {
         t /= d;
         return c * t * t + b;
     }
 
     function initScroll() {
+        if (!shouldScroll()) {
+            if(header.style.cssText ||  logo.style.cssText) {
+                header.style.cssText = '';
+                logo.style.cssText = '';
+            }
+            return;
+        }
         requestAnimationFrame(() => {
             let scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-            scrollTop = Math.max(scrollTop < maxScroll ? scrollTop : maxScroll, 0);
+                scrollTop = Math.max(scrollTop < maxScroll ? scrollTop : maxScroll, 0);
+
+            // 1) parallax calculations
+            const  opacityEase = easeInQuad(scrollTop, 0, 1, maxScroll),
+                   opacity   = 1 - opacityEase,
+                   move     = easeInQuad(scrollTop, 0, 50, maxScroll),
+                   moveIn   = 50 -easeInQuad(scrollTop, 0, 50, maxScroll),
+                   angel    = easeInQuad(scrollTop, 0, 10, maxScroll);
+
+            // 2) apply to styles
             header.style.cssText += `
                 transform:translate3d(0,-${scrollTop}px,0);
                 -webkit-transform:translate3d(0,-${scrollTop}px,0);`;
-            const opacity = 1 - easeInQuad(scrollTop, 0, 1, maxScroll),
-                move = easeInQuad(scrollTop, 0, 50, maxScroll);
 
             logo.style.cssText += `opacity:${opacity};
                 transform:translate3d(0,${move}px,0);
-                -webkit-transform:translate3d(0,${move}px,0);
-                `;
+                -webkit-transform:translate3d(0,${move}px,0);`;
+
+            logoBar.style.cssText += `
+            opacity:${opacityEase};
+            transform:translate3d(0,${moveIn}px,0) rotate(${angel*-1}deg);
+            -webkit-transform:translate3d(0,${moveIn}px,0) rotate(${angel*-1}deg);`;
         });
     }
 
     initScroll();
-
     global.window.addEventListener('scroll', initScroll, true);
-});
+})
+;
