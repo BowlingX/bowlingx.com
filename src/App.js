db83fcf55a042bdb33af3ba1c48a5e651299793a
David Heidrich <me@bowlingx.com>
2015-05-14 03:08:01 +0200

    request site-height dynamically

--- src/App.js
+++ src/App.js
@@ -9,56 +9,45 @@ global.document.addEventListener('DOMContentLoaded', () => {
     // init forms
     const
         desktopBreakpoint = 768,
-        isSmall           = window.innerWidth < desktopBreakpoint,
-        header          = isSmall ? document.getElementById('TopBar') : document.getElementById('Header'),
-        logoBar           = document.getElementById('Logo-bar'),
-        logo              = document.getElementById('Logo'),
-        hero              = document.getElementById('Hero'),
-        heroContent       = document.getElementById('HeroContent'),
-        headerNavHeight   = 50,
-        headerHeight      = header.getBoundingClientRect().height,
-        isFrontPage       = document.body.classList.contains('front-page'),
-        siteHeight        = window.innerHeight,
-        maxScroll         = isFrontPage? siteHeight + headerHeight - headerNavHeight : headerHeight - headerNavHeight,
+        isSmall = window.innerWidth < desktopBreakpoint,
+        header = isSmall ? document.getElementById('TopBar') : document.getElementById('Header'),
+        logoBar = document.getElementById('Logo-bar'),
+        logo = document.getElementById('Logo'),
+        hero = document.getElementById('Hero'),
+        heroContent = document.getElementById('HeroContent'),
+        headerNavHeight = 50,
+        headerHeight = header.getBoundingClientRect().height,
+        isFrontPage = document.body.classList.contains('front-page'),
         // easing vars:
-        scrollDownHeroDuration = isSmall ? headerHeight*10 : headerHeight * 2,
-        scrollDownHeroMax      = isSmall ? 60 : 320,
-        shouldScroll      = () => {
-            return true;
-        };
-
-    console.log(maxScroll);
+        scrollDownHeroDuration = isSmall ? headerHeight * 10 : headerHeight * 2,
+        scrollDownHeroMax = isSmall ? 60 : 320;
 
     // easing function to use for parallax effects
     function easeInQuad(t, b, c, d) {
         t /= d;
         return c * t * t + b;
     }
-    function easeOutQuad (t, b, c, d) {
+
+    function easeOutQuad(t, b, c, d) {
         return -c * (t /= d) * (t - 2) + b;
     }
 
     function initScroll() {
-        if (!shouldScroll()) {
-            if(header.style.cssText ||  logo.style.cssText) {
-                header.style.cssText = '';
-                logo.style.cssText = '';
-            }
-            return;
-        }
         requestAnimationFrame(() => {
+            const siteHeight = window.innerHeight,
+                maxScroll = isFrontPage ? siteHeight + headerHeight - headerNavHeight : headerHeight - headerNavHeight;
             let scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-                scrollTop = Math.max(scrollTop < maxScroll ? scrollTop : maxScroll, 0);
+            scrollTop = Math.max(scrollTop < maxScroll ? scrollTop : maxScroll, 0);
 
             // 1) parallax calculations
-            const  opacityEase = easeInQuad(scrollTop, 0, 1, maxScroll),
-                   opacity   = 1 - opacityEase,
-                   move     = easeInQuad(scrollTop, 0, headerNavHeight, maxScroll),
-                   moveIn   = headerNavHeight -easeInQuad(scrollTop, 0, headerNavHeight, maxScroll),
-                   angel    = easeInQuad(scrollTop, 0, 12, maxScroll);
+            const opacityEase = easeInQuad(scrollTop, 0, 1, maxScroll),
+                opacity = 1 - opacityEase,
+                move = easeInQuad(scrollTop, 0, headerNavHeight, maxScroll),
+                moveIn = headerNavHeight - easeInQuad(scrollTop, 0, headerNavHeight, maxScroll),
+                angel = easeInQuad(scrollTop, 0, 12, maxScroll);
 
             // a) if there exists an hero page before
-            if(hero && heroContent) {
+            if (hero && heroContent) {
                 const startZero = Math.max(scrollTop - headerHeight, 0),
                     scrollTopHero = isSmall ? startZero : easeOutQuad(startZero, 0, siteHeight, siteHeight * 1.2),
                     opacity = 1 - easeOutQuad(startZero, 0, 1, maxScroll);
@@ -84,13 +73,12 @@ global.document.addEventListener('DOMContentLoaded', () => {
 
             logoBar.style.cssText += `
             opacity:${opacityEase};
-            transform:translateY(${moveIn}px) rotate(${angel*-1}deg) scale(.93);
-            -webkit-transform:translateY(${moveIn}px) rotate(${angel*-1}deg) scale(.93);`;
+            transform:translateY(${moveIn}px) rotate(${angel * -1}deg) scale(.93);
+            -webkit-transform:translateY(${moveIn}px) rotate(${angel * -1}deg) scale(.93);`;
 
         });
     }
 
     initScroll();
     global.window.addEventListener('scroll', initScroll, true);
-})
-;
+});
